43c98081bea0b1304e1aa25c49feb61358642dd2
Roger Mullins <rogermullins.mba@gmail.com>
2022-09-07 11:55:08 +0000

    Added math conversions for weight and temp

--- main.js
+++ main.js
@@ -43,11 +43,12 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = (Math.round(300/14) + " stone");
+    const temperature = (Math.round((94-32)*5/9) + " centigrade");
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
